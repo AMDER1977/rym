9f333d6fc5aa3fa867fccac7bde717c09ad190d8
Amderson Hernandez <ahernandezdep@gmail.com>
2023-07-26 17:41:48 -0500

    "2da Homework Promises"

--- rick_and_morty/Server/src/index.js
+++ rick_and_morty/Server/src/index.js
@@ -1,17 +1,13 @@
 const http = require("http");
-const characters = require("../../Server/utils/data");
-const PORT = 3001;
+const getCharById = require("../controllers/getCharById");
 
 http
   .createServer((req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
+
     if (req.url.includes("/rickandmorty/character")) {
-      const id = req.url.split("/").pop();
-      let characterFilter = characters.filter(
-        (character) => character.id === Number(id)
-      );
-      res.writeHead(200, { "Content-type": "application/json" });
-      res.end(JSON.stringify(characterFilter));
+      let id = req.url.split("/").at(-1);
+      getCharById(res, id);
     }
   })
-  .listen(PORT, () => console.log("Server running on port 3001"));
+  .listen(3001, () => console.log("Port on 3001"));
