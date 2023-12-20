bc53b2f100543918a5bbf1291d56027133110133
Amderson Hernandez <ahernandezdep@gmail.com>
2023-12-19 20:50:12 -0500

    "check login"

--- rick_and_morty/Client/src/views/about/about.jsx
+++ rick_and_morty/Client/src/views/about/about.jsx
@@ -1,18 +1,20 @@
-import style from "./about.css";
+import "./about.css";
+
 function About() {
   return (
     <div className="aboutMe">
       <h1>Acerca de mí</h1>
 
       <p>
-        <img src="./ajhd.jpg" alt="Foto de mí en el Bootcamp de Henry"></img>
-        Mi Nombre es Amderson Hernandez, Licenciado en Educación y apasionado
-        por el campo de las TI, este es un proyecto personal desarrollado
-        durante mi cursada en el Bootcamp de Henry, utilizando Javascript,
-        React, Redux, los estilos con Css, a lo cual progresivamente se le iran
-        sumando las tecnologias que vaya adquiriendo como competencias dictadas
+        Mi Nombre es Amderson Hernandez, Licenciado en Educación , en continua
+        formacion y crecimiento en el campo de las TI, este es un proyecto
+        personal desarrollado durante mi cursada en el Bootcamp de Henry,
+        utilizando Javascript, React, Redux, los estilos con Css, Express,
+        sequalize, postgress SQL; a lo cual progresivamente se le iran sumando
+        otras tecnologias que vaya adquiriendo como competencias dictadas
         durante el Bootcamp
       </p>
+      <img src="./ajhd.jpg" alt="Foto de mí en el Bootcamp de Henry"></img>
     </div>
   );
 }
